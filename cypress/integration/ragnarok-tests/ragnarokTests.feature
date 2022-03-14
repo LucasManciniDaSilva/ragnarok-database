71cd95e44953f4e1ca7a553e25be9a5cccdcfb96
LucasManciniDaSilva <lucasmancini1997@gmail.com>
2022-03-14 14:30:21 -0300

    Create the first Scenario on Feature Ragnarok Database Tests

--- cypress/integration/ragnarok-tests/ragnarokTests.feature
+++ cypress/integration/ragnarok-tests/ragnarokTests.feature
@@ -1,6 +1,8 @@
 Feature: Ragnarok Database tests
 
 
-  Scenario: First Ragnarok Database Test
+  Scenario: Check the lenght of monsters in the database
     Given I am on the Ragnarok Database page
     When Check if the title of the page is "Database Oficial"
+    And Click on "Monstros" and check if the title of the page is "Monstros"
+    Then Check the lenght of the list of monsters is 12
--- cypress/integration/ragnarok-tests/ragnarokTests/ragnarokTestsSteps.js
+++ cypress/integration/ragnarok-tests/ragnarokTests/ragnarokTestsSteps.js
@@ -6,3 +6,15 @@ Given(/^I am on the Ragnarok Database page$/, () => {
 When(/^Check if the title of the page is "([^"]*)"$/, (title) => {
   cy.get("h1").should("contain", title);
 });
+
+And(
+  /^Click on "([^"]*)" and check if the title of the page is "([^"]*)"$/,
+  (button, title) => {
+    cy.get("a").contains(button).click();
+    cy.get("h1").should("contain", title);
+  }
+);
+
+Then("Check the lenght of the list of monsters is {int}", (lenght) => {
+  cy.get(".monstros.show").its("length").should("eq", lenght);
+});
